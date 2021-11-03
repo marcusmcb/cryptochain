9a164aee68ae7e5981a851524c31a0a988608a0a
Marcus McBride <marcusmcb@gmail.com>
2021-11-03 11:32:15 -0700

    configured production code

---
+++ client/src/components/ConductTransaction.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import { FormGroup, FormControl, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import history from '../history'
+
+class ConductTransaction extends Component {
+  state = { recipient: '', amount: 0 }
+
+  updateRecipient = (event) => {
+    this.setState({ recipient: event.target.value })
+  }
+
+  updateAmount = (event) => {
+    this.setState({ amount: Number(event.target.value) })
+  }
+
+  conductTransaction = () => {
+    const { recipient, amount } = this.state
+    fetch(`${document.location.origin}/api/transact`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ recipient, amount }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        alert(json.message || json.type)
+        history.push('/transaction-pool')
+      })
+  }
+
+  render() {
+    return (
+      <div className='ConductTransaction'>
+        <Link to='/'>Home</Link>
+        <h3>Conduct A Transaction</h3>
+
+        <FormGroup>
+          <FormControl
+            input='text'
+            placeholder='recipient'
+            value={this.state.recipient}
+            onChange={this.updateRecipient}
+          />
+        </FormGroup>
+        <br />
+        <FormGroup>
+          <FormControl
+            input='number'
+            placeholder='amount'
+            value={this.state.amount}
+            onChange={this.updateAmount}
+          />
+        </FormGroup>
+        <br />
+        <div>
+          <Button
+            className='btn btn-danger btn-small'
+            onClick={this.conductTransaction}
+          >
+            Submit
+          </Button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ConductTransaction
