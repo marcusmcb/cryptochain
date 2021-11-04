4428eecd19861c21f2e76f08f55b44b351afc870
Marcus McBride <marcusmcb@gmail.com>
2021-11-04 12:45:09 -0700

    updated babel/preset-react

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -52923,7 +52923,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56586" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53848" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
